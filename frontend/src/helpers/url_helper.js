517f50f5f4caf8408420e5f0fd46a75eebf22ee6
Abbas Kapasi <Abbas.Kapasi@esds.co.in>
2023-11-09 16:52:08 +0530

    initial commit

---
+++ frontend/src/helpers/url_helper.js
@@ -0,0 +1,16 @@
+//TASKS
+export const GET_TASKS = "/tasks";
+
+//CONTACTS
+export const GET_USERS = (id) => `/user/${id}`;
+export const GET_USER_PROFILE = (id) => `/user/getUser/${id}`;
+
+//USER AUTH
+export const REGISTER = `/user/sign-up`;
+export const LOGIN = `/user/login`;
+export const FORGET_PASSWORD = `/user/forgot-password`;
+export const RESET_PASSWORD = `/user/reset-password`;
+export const UPDATE_USER = `/user/profile`;
+export const REQUEST_BLOOD = `/user/emergency`;
+
+//(GET_USERS())
